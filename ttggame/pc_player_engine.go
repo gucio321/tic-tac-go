abafd44c1cc2b9395fa023917f01028356281b62
gucio321 <gucio321@protonmail.com>
2021-04-21 14:28:54 +0200

    wip: pcplayerengine: canwintwomoves
    - blocks user / makes move if the habe can be won in two moves ahead (horizontal)

--- ttggame/pc_player_engine.go
+++ ttggame/pc_player_engine.go
@@ -1,6 +1,7 @@
 package ttggame
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"time"
@@ -20,7 +21,7 @@ func (t *TTG) canWin(player ttgletter.Letter) (i int, result bool) {
 
 		board.SetIndexState(i, player)
 
-		if t.isWinner(board, player) {
+		if t.isWinner(board, t.chainLen, player) {
 			return i, true
 		}
 	}
@@ -28,27 +29,123 @@ func (t *TTG) canWin(player ttgletter.Letter) (i int, result bool) {
 	return 0, false
 }
 
-func (t *TTG) canWinTwoMoves(player ttgletter.Letter) (i int, result bool) {
-	for i := 0; i < t.width*t.height; i++ {
-		/*if t.board.GetIndexState(i) != player {
-			continue
-		}
-
-		if ttgcommon.IsEdgeIndex(t.width, t.height, i) {
-			continue
-		}*/
-
-		// board := t.board.Copy().Cut(t.width-2, t.height-2)
-
-		// minimalChainLen := t.chainLen - 2
-		// ...
-
-		/*if t.isWinner(board, player) {
-			return i, true
-		}*/
+/*
+This method should find situations like that:
+chain lenght = 4
++---+---+---+---+---+
+|   |   | o |   |   |
++---+---+---+---+---+
+|   |   |   |   | o |
++---+---+---+---+---+
+|   | x | x |   |   |
++---+---+---+---+---+
+|   |   |   |   |   |
++---+---+---+---+---+
+|   |   |   | o |   |
++---+---+---+---+---+
+let's look at bove board: when we'll make our move at left side of X-chain (14)
+the O-player will not be able to keep X from winning.
++---+---+---+---+---+
+|   |   | o |   |   |
++---+---+---+---+---+
+|   |   |   |   | o |
++---+---+---+---+---+
+|   | x | x | X |   |
++---+---+---+---+---+
+|   |   |   |   |   |
++---+---+---+---+---+
+|   |   |   | o |   |
++---+---+---+---+---+
+O-player lost.
+*/
+func (t *TTG) canWinTwoMoves(player ttgletter.Letter) (result []int) {
+	minimalChainLen := t.chainLen - 2
+	// processing this doesn't make sense
+	if minimalChainLen == 1 {
+		return nil
 	}
 
-	return 0, false
+	b := ttgcommon.GetWinBoard(t.board.Width(), t.board.Height(), minimalChainLen)
+
+	options := make([][]int, 0)
+
+	for _, i := range b {
+		line := 0
+
+		for _, c := range i {
+			if t.board.GetIndexState(c) == player {
+				line++
+			}
+		}
+
+		if line == minimalChainLen {
+			options = append(options, i)
+		}
+	}
+
+	for _, option := range options {
+		/*row := make([]int, len(option))
+		for n := range option {
+			row[n] = ttgcommon.ConvertIndex(
+				board.Width(),
+				board.Height(),
+				t.board.Width(),
+				t.board.Width(),
+				option[n],
+			)
+		}
+		*/
+		switch option[0] {
+		case option[1] - 1: // horizontal
+			separator := 1
+			if option[0]-separator < 0 {
+				continue
+			}
+
+			if !t.board.IsIndexFree(option[0] - separator) {
+				continue
+			}
+
+			if option[len(option)-1]+separator >= t.width*t.height {
+				continue
+			}
+
+			if !t.board.IsIndexFree(option[len(option)-1] + separator) {
+				continue
+			}
+
+			// special case
+			if (option[0]+1-1)%t.width == 0 { // will move back to the previous line
+				continue
+			}
+
+			if (option[len(option)-1]+1)%t.width == 1 { // will move back to the previous line
+				continue
+			}
+
+			if index := option[0] - 2*separator; index > 0 {
+				if t.board.IsIndexFree(index) &&
+					(index+1)%t.width != 0 {
+					result = append(result, index+separator)
+				}
+			}
+			if index := option[len(option)-1] + 2*separator; index < t.width*t.height {
+				if t.board.IsIndexFree(index) &&
+					(index+1)%t.width != 1 {
+					result = append(result, index-separator)
+				}
+			}
+			// index := option[len(option)-1]
+			fmt.Println(t.width, ((option[len(option)-1]+2)+1)%t.width)
+		case option[1] - t.board.Width(): // vertical
+		case option[1] - t.board.Width() + 1: // slant - forward
+		case option[1] - t.board.Width() - 1: // slant - backword
+		default: // shouldn't be reached
+			fmt.Println("Nothing batches: ", option)
+		}
+	}
+
+	return result
 }
 
 func (t *TTG) getPCMove(letter ttgletter.Letter) (i int) {
@@ -67,6 +164,33 @@ func (t *TTG) getPCMove(letter ttgletter.Letter) (i int) {
 	// defense: check, if user can win
 	if i, ok := t.canWin(playerLetter); ok {
 		return i
+	}
+
+	for _, i := range t.canWinTwoMoves(pcLetter) {
+		if t.board.IsIndexFree(i) {
+			options = append(options, i)
+		}
+	}
+
+	if options != nil {
+		// nolint:gosec // it's ok
+		result := options[rand.Intn(len(options))]
+
+		return result
+	}
+
+	for _, i := range t.canWinTwoMoves(playerLetter) {
+		if t.board.IsIndexFree(i) {
+			options = append(options, i)
+			fmt.Println(i)
+		}
+	}
+
+	if options != nil {
+		// nolint:gosec // it's ok
+		result := options[rand.Intn(len(options))]
+
+		return result
 	}
 
 	const doubbleRow = 2
