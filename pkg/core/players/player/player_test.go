14b60b964c7798f083b83bfbdb5c9c02f2e6a377
gucio321 <gucio321@protonmail.com>
2021-10-12 19:20:59 +0200

    player.go: add missing test; rewrite to use `assert`

--- pkg/core/players/player/player_test.go
+++ pkg/core/players/player/player_test.go
@@ -4,50 +4,43 @@ import (
 	"testing"
 
 	"github.com/gucio321/tic-tac-go/pkg/core/board/letter"
+	"github.com/stretchr/testify/assert"
 )
 
 const playerString = "Player X"
 
 func Test_Create(t *testing.T) {
-	player := Create(PlayerPerson, letter.LetterX, func(_ letter.Letter) int { return 8 })
+	const num = 8
+	a := assert.New(t)
+	player := Create(PlayerPerson, letter.LetterX, func(_ letter.Letter) int { return num })
 
-	if player.playerType != PlayerPerson {
-		t.Fatal("Unexpected player created")
-	}
-
-	if player.letter != letter.LetterX {
-		t.Fatal("Unexpected player created")
-	}
-
-	if player.moveCb(letter.LetterX) != 8 {
-		t.Fatal("Unexpected player created")
-	}
-
-	if player.name != playerString {
-		t.Fatal("Unexpected player created")
-	}
+	a.Equal(PlayerPerson, player.playerType, "Unexpected player created")
+	a.Equal(letter.LetterX, player.letter, "Unexpected player created")
+	a.Equal(num, player.moveCb(player.letter), "Unexpected player created")
+	a.Equal(playerString, player.name, "Unexpected player created")
 }
 
 func Test_Move(t *testing.T) {
-	player := Create(PlayerPerson, letter.LetterX, func(_ letter.Letter) int { return 8 })
+	const num = 8
+	player := Create(PlayerPerson, letter.LetterX, func(_ letter.Letter) int { return num })
 
-	if player.Move() != 8 {
-		t.Fatal("unexpected move done")
-	}
+	assert.Equal(t, num, player.Move(), "unexpected move done")
 }
 
 func Test_Letter(t *testing.T) {
 	player := Create(PlayerPerson, letter.LetterX, nil)
 
-	if player.Letter() != letter.LetterX {
-		t.Fatal("unexpected letter returned")
-	}
+	assert.Equal(t, letter.LetterX, player.Letter(), "unexpected letter returned")
 }
 
 func Test_Name(t *testing.T) {
 	player := Create(PlayerPerson, letter.LetterX, nil)
 
-	if player.Name() != playerString {
-		t.Fatal("unexpected name returned")
-	}
+	assert.Equal(t, playerString, player.Name(), "unexpected name returned")
+}
+
+func Test_Type(t *testing.T) {
+	player := Create(PlayerPerson, letter.LetterX, nil)
+
+	assert.Equal(t, PlayerPerson, player.Type(), "Unexpected type of the player returned")
 }
