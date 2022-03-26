87c12f0ef23da8d5ac1f0acf5aa38e8c1f2a26f0
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-03-26 19:42:18 +0100

    scripts: improve draw-board script

--- scripts/draw-board/main.go
+++ scripts/draw-board/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/gucio321/tic-tac-go/pkg/core/board/letter"
+	"strconv"
 
 	"github.com/gucio321/tic-tac-go/pkg/core/board"
 )
@@ -12,10 +14,37 @@ const (
 	defaultChainLen  = 3
 )
 
+var _ flag.Value = &lettersValue{}
+
+type lettersValue []int
+
+func (l *lettersValue) String() string {
+	return fmt.Sprintf("%v", *l)
+}
+
+func (l *lettersValue) Set(value string) (err error) {
+	for _, letter := range value {
+		if letter < '0' || letter > '9' {
+			return fmt.Errorf("invalid letter: %v", letter)
+		}
+	}
+
+	x, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+
+	*l = append(*l, x)
+
+	return nil
+}
+
 type flags struct {
 	width    *int
 	height   *int
 	chainLen *int
+	x        lettersValue
+	o        lettersValue
 }
 
 func parseFlags() *flags {
@@ -23,6 +52,8 @@ func parseFlags() *flags {
 	result.width = flag.Int("width", defaultBoardSize, "width of the board")
 	result.height = flag.Int("height", defaultBoardSize, "height of the board")
 	result.chainLen = flag.Int("chain-len", defaultChainLen, "length of the chain to win")
+	flag.Var(&result.x, "x", "indexes with X")
+	flag.Var(&result.o, "o", "indexes with O")
 	flag.Parse()
 
 	return result
@@ -31,11 +62,19 @@ func parseFlags() *flags {
 func main() {
 	f := parseFlags()
 
-	fmt.Println(
-		board.Create(
-			*f.width,
-			*f.height,
-			*f.chainLen,
-		),
+	b := board.Create(
+		*f.width,
+		*f.height,
+		*f.chainLen,
 	)
+
+	for _, x := range f.x {
+		b.SetIndexState(x, letter.LetterX)
+	}
+
+	for _, o := range f.o {
+		b.SetIndexState(o, letter.LetterO)
+	}
+
+	fmt.Println(b)
 }
